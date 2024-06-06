d5140e9842c7924184d2c86851c756afb834c559
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-06-06 11:11:08 +0500

    .

---
+++ index.js
@@ -0,0 +1,11 @@
+//1-masala
+// let a = +prompt("1- sonni kiriting");
+// let b = +prompt("2- sonni kiriting");
+// let c = +prompt("3- sonni kiriting");
+// if (a > b || (b > c && c > 0)) {
+//   console.log("shart bajarildi");
+// } else {
+//   console.log("shart bajarilmadi");
+// }
+
+//2-masala
