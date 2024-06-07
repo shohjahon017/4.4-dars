60e6ba0f4491e037d55783ca1ad9bee58e30450e
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-06-07 18:57:18 +0500

    ..

--- index.js
+++ index.js
@@ -8,4 +8,224 @@
 //   console.log("shart bajarilmadi");
 // }
 
-//2-masala
+//3-masala
+// let a = +prompt("1- sonni kiriting");
+// let b = +prompt("2- sonni kiriting");
+// let c = +prompt("3- sonni kiriting");
+// let d = +prompt("4- sonni kiriting");
+// if (a + b + c + d = 50 && a > 10 || b > 10 || c > 10 || d > 10) {
+//      console.log('shart bajarildi');
+// }else{
+//     console.log('shart bajarilmadi');
+// }
+
+//4-masala
+// let a = +prompt("1-sonni kirting");
+// let b = +prompt("2-sonni kiriting");
+// if (a * b < 100 || (a + b > 20 && a % 2 == 0)) {
+//   console.log("shart bajarildi");
+// } else {
+//   console.log("shart bajarilmadi");
+// }
+
+//6-masala
+// let a = +prompt("1-sonni kirting");
+// let b = +prompt("2-sonni kiriting");
+// if ((a % b == 0 && b > 0) || b % 5 == 0) {
+//   co nsole.log("shart bajarildi");
+// }else{
+//  console.log("shart bajarilmadi");
+// }
+
+//7-masala
+// let a = +prompt("1- sonni kiriting");
+// let b = +prompt("2- sonni kiriting");
+// let c = +prompt("3- sonni kiriting");
+// if ((a > 0 && b > 0 && c > 0 && b < a * a) || c % 2 == 1) {
+//   console.log("shart bajarildi");
+// } else {
+//   console.log("shart bajarilmadi");
+// }
+
+//9-masala
+// let a = +prompt("1- sonni kiriting");
+// let b = +prompt("2- sonni kiriting");
+// let c = +prompt("3- sonni kiriting");
+// if (((a + b + c) / 3 > 10 && a > b) || c < 15) {
+//   console.log("shart bajarildi ");
+// } else {
+//   console.log("shart bajarilmadi");
+// }
+
+//11-masala
+// let a = +prompt("a sonni kiriting");
+// a > 0 ? console.log("musbat") : console.log("manfiy");
+
+// 12-masala
+// let a = +prompt("1- sonni kiriting");
+// let b = +prompt("2- sonni kiriting");
+// a > b ? console.log("1-son katta") : console.log("2-son katta");
+
+// 14 - masala;
+// let a = +prompt("a sonni kiriting");
+// a % 2 == 0 ? console.log("juft") : console.log("toq");
+
+//15-masala
+// let a = +prompt("a sonni kiriting");
+// a > 0 || a <= 0
+//   ? console.log("musbat son")
+//   : console.log("nol yoki manfiy son");
+
+//16-masala
+// let a = +prompt("a sonni kiriting");
+// switch (a) {
+//   case 1:
+//     console.log("dushanba");
+//     break;
+//   case 2:
+//     console.log("seshanba");
+//     break;
+//   case 3:
+//     console.log("chorshanba");
+//     break;
+//   case 4:
+//     console.log("payshanba");
+//     break;
+//   case 5:
+//     console.log("juma");
+//     break;
+//   case 6:
+//     console.log("shanba");
+//     break;
+//   case 7:
+//     console.log("yakshanba");
+//     break;
+//   default:
+//     console.log("Bunday hafta kuni mavjud emas");
+//     break;
+// }
+
+//17-masala
+// let a = +prompt("qaysi faslga tugri keladi");
+// switch (a) {
+//   case 1:
+//     console.log("yanvar");
+//     break;
+//   case 2:
+//     console.log("fevral");
+//     break;
+//   case 3:
+//     console.log("mart");
+//     break;
+//   case 4:
+//     console.log("aprel");
+//     break;
+//   case 5:
+//     console.log("may");
+//     break;
+//   case 6:
+//     console.log("iyun");
+//     break;
+//   case 7:
+//     console.log("iyul");
+//     break;
+//   case 8:
+//     console.log("avgust");
+//     break;
+//   case 9:
+//     console.log("sentabr");
+//     break;
+//   case 10:
+//     console.log("oktabr");
+//     break;
+//   case 11:
+//     console.log("noyabr");
+//     break;
+//   case 12:
+//     console.log("dekabr");
+//     break;
+
+//   default:
+//     console.log("Bunday oy mavjud emas");
+//     break;
+// }
+
+//18-masala
+// let a = +prompt("sonni kiriting");
+// switch (a) {
+//   case A:
+//     console.log("A`lo");
+//     break;
+//   case B:
+//     console.log("Yaxshi");
+//     break;
+//   case C:
+//     console.log("");
+//     break;
+//   case D:
+//     console.log("A`lo");
+//     break;
+//   case F:
+//     console.log("A`lo");
+//     break;
+//   default:
+//console.log("noma`lum qiymat");
+//     break;
+// }
+
+// 19-masala
+// let a = +prompt("sonni kiriting");
+// switch (a) {
+//   case 12:
+//   case 1:
+//   case 2:
+//     console.log("Qish");
+//     break;
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log("Bahor");
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     console.log("Yoz");
+//     break;
+//   case 9:
+//   case 10:
+//   case 11:
+//     console.log("Kuz");
+//     break;
+//   default:
+//     console.log("Bunday fasl mavjud emas");
+//     break;
+// }
+
+//20-masala
+// let a = +prompt("sonni kiriting");
+// switch (a) {
+//   case 1:
+//     console.log("birinchi");
+//     break;
+//   case 2:
+//     console.log("Ikkinchi");
+//     break;
+//   case 3:
+//     console.log("uchinchi");
+//     break;
+//   case 4 :
+//     console.log("turtinchi");
+//     break;
+//   case 5:
+//     console.log("besinchi");
+//     break;
+//   default:
+//console.log("noma`lum qiymat");
+//     break;
+// }
+
+//21-masala
+for (let i = 10; i <= 1; i++) {
+  console.log(i);
+}
+
